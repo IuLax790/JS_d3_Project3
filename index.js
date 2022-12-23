4ceb140595bf3cd6642dc39f14c46a998ecb0727
Ido Koubi <66007024+IuLax790@users.noreply.github.com>
2022-12-23 22:27:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React, { useState, useCallback, useEffect } from 'react';
 import ReactDOM from 'react-dom';
-import { csv, arc, pie, scaleBand, scaleLinear, max } from 'd3';
+import { csv, arc, pie, scaleBand, scaleLinear, max, format } from 'd3';
 import { useData } from './useData';
 import { AxisBottom } from './AxisBottom';
 import { AxisLeft } from './AxisLeft';
@@ -8,7 +8,8 @@ import { Marks } from './Marks';
 
 const width = 960;
 const height = 500;
-const margin = { top: 20, right: 20, bottom: 20, left: 200 };
+const margin = { top: 20, right: 30, bottom: 65, left: 220 };
+const xAxisLabelOffset = 50;
 
 const App = () => {
   const data = useData();
@@ -23,9 +24,13 @@ const App = () => {
   const yValue = d => d.Country;
   const xValue = d => d.Population;
 
+  const siFormat = format('.2s');
+  const xAxisTickFormat = tickValue => siFormat(tickValue).replace('G', 'B');
+
   const yScale = scaleBand()
     .domain(data.map(yValue))
-    .range([0, innerHeight]);
+    .range([0, innerHeight])
+    .paddingInner(0.15);
 
   const xScale = scaleLinear()
     .domain([0, max(data, xValue)])
@@ -34,14 +39,27 @@ const App = () => {
   return (
     <svg width={width} height={height}>
       <g transform={`translate(${margin.left},${margin.top})`}>
-        <AxisBottom xScale={xScale} innerHeight={innerHeight} />
+        <AxisBottom
+          xScale={xScale}
+          innerHeight={innerHeight}
+          tickFormat={xAxisTickFormat}
+        />
         <AxisLeft yScale={yScale} />
+        <text
+          className="axis-label"
+          x={innerWidth / 2}
+          y={innerHeight + xAxisLabelOffset}
+          textAnchor="middle"
+        >
+          Population
+        </text>
         <Marks
           data={data}
           xScale={xScale}
           yScale={yScale}
           xValue={xValue}
           yValue={yValue}
+          tooltipFormat={xAxisTickFormat}
         />
       </g>
     </svg>
